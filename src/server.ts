637b94030ad32343de63a95a5c4da9ba58839fbc
Dino Tudor <dinotudor@gmail.com>
2020-07-12 20:03:56 -0300

    TypeORM Config, installation

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import express from 'express';
 
 import routes from './routes';
 
-// const PORT = 3333;
+import './database';
+
+const PORT = 3333;
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log(`Server started at port: 3333`);
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
 });
